d6a8076f8e0f45ff85b7f380e117d29acd864517
kim gi chul <21500048@handong.edu>
2018-04-21 00:59:07 +0900

    complete Lab09

--- src/main/java/handong/edu/csee/java/Lab09/SalesReporter.java
+++ src/main/java/handong/edu/csee/java/Lab09/SalesReporter.java
@@ -94,60 +94,64 @@ public class SalesReporter { // Declare the class SalesReporter
 	/**
 	 * This method calculate the average of sales and store the value in the variable averageSales
 	 */
-	public void calculateAverageSales() {
-		double sum=0; 
+	public void calculateAverageSales() { //Declare the method calculateAverageSales
+		double sum=0; //Declare the double type variable sum, and initialize it to 0
 
-		for(int i=0; i<team.length; i++) 
+		for(int i=0; i<team.length; i++) //Add all of sales by iteration
 		{
-			double sales= team[i].getSales(); 
-			sum=sum+sales; 
+			double sales= team[i].getSales(); //Declare the variable and store ith salesman's sales in the variable  
+			sum=sum+sales; //Accumulate the value of sales 
 		}
-		averageSales = sum/team.length; 
+		averageSales = sum/team.length; // Addition of all sales devide by number of salesman
 	}
 
-
-	public void highestSales() { 
+	/**
+	 * This method check who is man of highest sales, and store the highest sales in the variable highestSales
+	 * It compare all the sales of members of array by using iteration
+	 */
+	public void highestSales() { //Declare the method highestSales
 		
-		for(int i=0; i<team.length-1;i++) { 
-			if(team[i].getSales()<team[i+1].getSales()) { 
-				highestSales=team[i+1].getSales(); 
+		for(int i=0; i<team.length-1;i++) { // compare all the sales of salesman by iteration
+			if(team[i].getSales()<team[i+1].getSales()) { //compare i th salesman with (i+1) th salesman, and if (i+1)th salesman's sale is bigger
+				highestSales=team[i+1].getSales(); // highest sales store the (i+1)th salesman's sales 
 			}
 		}
 		
 	}
 
-	
-	
-	
-	public void printOutResults() { 
+	/**
+	 * This method print out the result
+	 * The result include the average, highest sales and salesman with highest sales, the other salesmen's data
+	 */
+	public void printOutResults() { //Declare the method printOutResult
 
 		System.out.println("Average sales per associate is $" + averageSales); //Print out the averageSales
 		System.out.println("Highest sales figure is $"+ highestSales); //Print out the highestSales
 		
 		System.out.println("The Highest sales guy:"); //Print out the highest salesman's data
 
-		for(int i=0; i<team.length; i++) { 
-			
-			if(team[i].getSales() == highestSales) {
-				System.out.println("Name : "+team[i].getmName()); 
-				System.out.println("Team : "+team[i].getSales()); 
+		for(int i=0; i<team.length; i++) { //Check the highest salesman by using iteration
+
+			if(team[i].getSales() == highestSales) {//If find the man with highest value
+				System.out.println("Name : "+team[i].getmName()); //Print out name of the man
+				System.out.println("Team : "+team[i].getSales()); //Print out sales of the man
 			}
 		}
 
-		System.out.println("The Rest performed as follow: ");
-		
-		for(int i=0; i<team.length; i++) { 
-			if(team[i].getSales() != highestSales) {
-				System.out.println("Name : "+team[i].getmName()); 
-				System.out.println("Sales : "+team[i].getSales()); 
+		System.out.println("The Rest performed as follow: "); //Print out the rest performed data
+
+		for(int i=0; i<team.length; i++) { //all the member(salesman) of Array print out by using iteration
+			if(team[i].getSales() != highestSales) { //In condition excluding the men with highest sales
+				System.out.println("Name : "+team[i].getmName()); //Print out the name of salesman
+				System.out.println("Sales : "+team[i].getSales()); //Print out the sales of salesman
 				
-				double temp = averageSales - team[i].getSales();
-				if(temp>0) 
-					System.out.println(Math.abs(temp)+ " below the average."); 
-				else if(temp<0) 
-					System.out.println(Math.abs(temp)+ " above the average."); 
-				else
-					System.out.println("equivalent to the average."); 
+				double temp = averageSales - team[i].getSales(); // This variable stored subtraction ith salesman's sales from average
+				if(temp>0) // If average is bigger than sales 
+					System.out.println(Math.abs(temp)+ " below the average."); //Print out the sales with message "below the average"
+				else if(temp<0) //If average is smaller than sales
+					System.out.println(Math.abs(temp)+ " above the average."); //Print out the sales with message "above the average"
+				else// If average is same with sales
+					System.out.println("equivalent to the average."); //Print out the sales with message "equivalent to the average"
 			}
 		}
 	}
@@ -155,38 +159,56 @@ public class SalesReporter { // Declare the class SalesReporter
 }
 
 
-
-
+/**
+ * This class had salesman's constructor and getter and setter of its member.
+ * 
+ * @author gichul
+ *
+ */
 class Salesman { //Declare the class Salesman
 
-	private String mName; 
-	private double sales; 
-	
+	private String mName; // Declare the string type variable mName, where salesmen's names are stored in
+	private double sales; //Declare the double type variable sales, where sales are stored in
 
-	public Salesman() {
-		   mName = ""; 
-		   sales = 0;
-	}
-	
-
-	public String getmName() { 
-		return mName; 
-	}
-	
-
-	
-	public void setmName(String mName) { 
-		this.mName = mName; 
+	/**
+	 * This is constructor of the class Salesman
+	 * The member of this is mName, salesman's name and sales, salesman's sales
+	 */
+	public Salesman() { //Declare the constructor of the class Salesman
+		mName = ""; //mName is salesman's name and initialized to ""
+		sales = 0; //sales is sales of salesman and initialized to 0 
 	}
 
-
-	public double getSales() { 
-		return sales; 
+	/**
+	 * This method is getter of mName
+	 * @return mName, salesman's name
+	 */
+	public String getmName() { //Declare the getter of mName
+		return mName; //return the mName
 	}
 
+	/**
+	 * This method is setter of mName
+	 * @param mName, salesman's name
+	 */
+	public void setmName(String mName) { //Declare the setter of mName
+		this.mName = mName; // Set the mName
+	}
 
-	public void setSales(double sales) { 
-		this.sales = sales; 
+	/**
+	 * This method is getter of Sales
+	 * @return Sales, sales of salesman
+	 */
+	public double getSales() { //Declare the getter of sales
+		return sales; //Return the sales
+	}
+
+	/**
+	 * This method is setter of Sales
+	 * @param sales, sales of salesman
+	 */
+	public void setSales(double sales) { //Declare the setter of sales
+		this.sales = sales; //set the sales
 	}
 
 }
